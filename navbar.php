15edfb44129bd75453dd35045f4ba830b09e461f
Shivam <shivam.kumar@imsuc.ac.in>
2022-02-11 09:31:58 +0530

    initial commit

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #495057;"><img src="img/logo.png" alt="Logo" width="30" style="margin-bottom:4px;"> <b>Spark Bank Of Commerce</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #1EA6C6;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #1EA6C6;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="removeuser.php" style="color : #1EA6C6;"><b>Remove User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #1EA6C6;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #1EA6C6;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
